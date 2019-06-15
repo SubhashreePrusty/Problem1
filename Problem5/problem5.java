ffce39f4c7f909dd2c769a78f6e1bddb0bfe3abc
SubhasreePrusty <sprusty0103@gmail.com>
2019-06-15 19:54:07 +0530

    completed problem 5

--- Problem5/problem5.java
+++ Problem5/problem5.java
@@ -1,33 +1,11 @@
-
-/**
- * Write a description of class problem5 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class problem5
+class problem5
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class problem5
-     */
-    public problem5()
+    public void main()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        int i=1;
+        while(i%1!=0 || i%2!=0 || i%3!=0 || i%4!=0 || i%5!=0 || i%6!=0 || i%7!=0 || i%8!=0 || i%9!=0 || i%10!=0 || i%11!=0 || i%12!=0 || i%13!=0 || i%14!=0 || i%15!=0 || i%16!=0 || i%17!=0 || i%18!=0 || i%19!=0 || i%20!=0)
+        {i++;
+        }
+        System.out.println("smallest possible no="+i);
     }
 }
